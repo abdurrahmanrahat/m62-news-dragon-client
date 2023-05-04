258182768a4913eb140a404ac04634ce3b325189
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-04 17:40:24 +0600

    Change url

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
             {
                 path: ":id",
                 element: <Category></Category>,
-                loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({ params }) => fetch(`https://m62-news-dragon-server-abdurrahmanrahat.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -51,7 +51,7 @@ const router = createBrowserRouter([
             {
                 path: ":id",
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({ params }) => fetch(`https://m62-news-dragon-server-abdurrahmanrahat.vercel.app/news/${params.id}`)
             }
         ]
     }
